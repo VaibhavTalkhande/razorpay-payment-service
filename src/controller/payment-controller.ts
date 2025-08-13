dee70175301e6156f32d71aab155c95734b2441e
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2025-08-13 21:32:51 +0530

    verification controller

--- src/controller/payment-controller.ts
+++ src/controller/payment-controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
-
 const {razorpay} = require("../utils/razorpay");
-
+const crypto = require('crypto');
 var options = {
   amount: 50000,  // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
   currency: "INR",
@@ -18,6 +17,25 @@ async function createRazorpayOrder(req:Request,res:Response) {
     }
 }
 
+function verifyPayment(req:Request,res:Response){
+    try{
+        const {razorpay_order_id,razorpay_payment_id,razorpay_signature}=req.body;
+        const sign = razorpay_order_id + "|" + razorpay_payment_id;
+        const expectedSign= crypto.createHmac("sha256",process.env.RAZORPAY_SECRET).update(sign).digest("hex");
+        if(razorpay_signature === expectedSign){
+            return res.json({message:"Payment verified successfully"});
+        }
+        else{
+            return res.status(400).json({message:"Payment verification failed"});
+        }
+    }
+    catch(error){
+        console.log("razorpay error",error)
+        return res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
 module.exports={
-    createRazorpayOrder
+    createRazorpayOrder,
+    verifyPayment
 }
