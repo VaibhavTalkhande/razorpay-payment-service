8acef89eb2a313758e7709987414486835999ab4
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2025-08-13 21:34:29 +0530

    route for verify-payment

--- src/routes/payments.ts
+++ src/routes/payments.ts
@@ -1,7 +1,7 @@
 const express = require("express");
-const {createRazorpayOrder} = require('../controller/payment-controller');
+const {createRazorpayOrder,verifyPayment} = require('../controller/payment-controller');
 const router = express.Router();
 
 router.post("/create-order",createRazorpayOrder);
-
+router.post("/verify-payment",verifyPayment);
 module.exports = router;
