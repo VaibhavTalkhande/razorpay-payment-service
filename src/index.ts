3b6fac427d8214907e0d459c6a5f752b39185e7d
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2025-08-15 22:04:07 +0530

    working on webhook integration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+const { validateWebhookSignature } =require("razorpay");
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -13,6 +14,37 @@ app.use(cors(
         origin: '*',
     }
 ))
+
+app.post('/api/webhook', (req: Request, res: Response) => {
+    const webhookSecret = process.env.RAZORPAY_WEBHOOK_SECRET;
+    if (!webhookSecret) {
+        throw new Error("RAZORPAY_WEBHOOK_SECRET is not defined in environment variables.");
+    }
+    const isValid = validateWebhookSignature(
+      JSON.stringify(req.body),
+      req.headers['x-razorpay-signature'] as string,
+      webhookSecret
+    );
+     if (isValid) {
+      const { event, payload } = req.body;
+
+      switch (event) {
+        case "payment.authorized":
+          console.log("Payment authorized:", payload);
+          break;
+        case "payment.captured":
+          console.log("Payment captured:", payload);
+          break;
+        case "payment.failed":
+          console.log("Payment failed:", payload);
+          break;
+        default:
+           console.log(`Unhandled event: ${event}`);
+          break;
+      }
+    }
+   res.status(200).send();
+});
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
